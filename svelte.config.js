837a7598dc7caf7951e03b35b2b01818bd5b2f59
utterfailure <071107nw@gmail.com>
2024-08-02 23:58:22 +1000

    V2. Changed base variable in svelte.config.js, ran npm run build

--- svelte.config.js
+++ svelte.config.js
@@ -9,13 +9,13 @@ const config = {
 	preprocess: [vitePreprocess()],
 	
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? 'whyiseverythingoutofstock.github.io' : '',
-        }
+			base: '/whyiseverythingoutofstock.github.io'
+		}
 	}
 };
 export default config;
+
